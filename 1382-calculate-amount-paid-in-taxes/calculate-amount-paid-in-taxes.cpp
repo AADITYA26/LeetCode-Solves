4ea2ba58608e38f0cff91df3230cff9d1ee3acdd
AADITYA26 <dhannawataaditya@gmail.com>
2026-08-06 22:14:41 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.29%) - LeetSync

---
+++ 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double calculateTax(vector<vector<int>>& brackets, int income) {
+        double tax = 0 ;
+        double prevupper = 0;
+        for(int i = 0 ; i < brackets.size() ; i++){
+            double upper = brackets[i][0];
+            double percent = brackets[i][1];
+            if(income > prevupper){
+                if(income > upper)tax+=(upper-prevupper)*percent/100;
+                else tax+=(income-prevupper)*percent/100;
+            }
+            prevupper = upper;
+        }
+        return tax;
+    }
+};
