faa028c58e8c4f07a013622b41bd0233b7d9218f
AADITYA26 <dhannawataaditya@gmail.com>
2026-08-14 16:11:44 +0530

    Time: 23 ms (99.86%) | Memory: 34 MB (85.75%) - LeetSync

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int target, int i,
-               vector<vector<int>>& dp) {
-        
-        if (target == 0)
-            return 0;
-
-        if (i == nums.size() || target < 0)
-            return -1;
-
-        if (dp[i][target] != -2)
-            return dp[i][target];
-
-        int take = helper(nums, target - nums[i], i + 1, dp);
-        if (take != -1)
-            take++;
-
-        int skip = helper(nums, target, i + 1, dp);
-
-        return dp[i][target] = max(take, skip);
-    }
-
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-        int n = nums.size();
+        vector<int> dp(target + 1, -1);
+        dp[0] = 0;
 
-        vector<vector<int>> dp(n, vector<int>(target + 1, -2));
+        for (int x : nums) {
+            for (int t = target; t >= x; t--) {
+                if (dp[t - x] != -1)
+                    dp[t] = max(dp[t], dp[t - x] + 1);
+            }
+        }
 
-        return helper(nums, target, 0, dp);
+        return dp[target];
     }
 };
